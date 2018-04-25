b6149726b381365bc59d4ab932c72b2c5982baf7
Антон Степаненков <Lenseg1@gmail.com>
2018-04-24 23:45:18 -0500

    history display

--- src/components/HistoryPage.vue
+++ src/components/HistoryPage.vue
@@ -3,32 +3,29 @@
     <div class="section">
       <div class="container">
         <h1 class="title">Transaction history</h1>
+        <ul class="transactions">
+          <li class="box" v-for="transaction in processedTransactions">
+            <div class="columns">
+              <div class="column is-10">
+                <h2 class="subtitle">
+                  {{transaction.timestamp}}
+                </h2>
+                <p>
+                  <span v-if="transaction.recieve">From: </span>
+                  <span v-if="!transaction.recieve">To: </span>
+                  {{transaction.address}}
+                </p>
+              </div>
+              <div class="column is-2">
+                <p>{{transaction.value}} ETH</p>
+              </div>
+            </div>
+            <div class="columns">
+              <p class="column is-12">{{transaction.hash}}</p>
+            </div>
+          </li>
+        </ul>
       </div>
-    </div>
-
-    <div class="section">
-      <ul>
-        <li v-for="transaction in processedTransactions" :class="{}">
-          <div class="columns">
-            <div class="column is-10">
-              <h2>
-                {{transaction.timestamp}}
-              </h2>
-              <p>
-                <span v-if="transaction.recieve">From: </span>
-                <span v-if="!transaction.recieve">To: </span>
-                {{transaction.address}}
-              </p>
-            </div>
-            <div class="column is-2">
-              <p>{{transaction.value}} ETH</p>
-            </div>
-          </div>
-          <div class="columns">
-            <p class="column is-12">{{transaction.hash}}</p>
-          </div>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -47,8 +44,12 @@ export default {
       });
       return sortedTransactions.map(trx => {
         let processedTrs = {};
-        const date = new Date(trx.timestamp*1000)
-        processedTrs.timestamp = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+        const date = new Date(trx.timestamp*1000);
+        let day = (date.getDay() < 10 ? '0' : '' ) + date.getDay();
+        let month = (date.getMonth() < 10 ? '0' : '' ) + date.getMonth();
+        let hours = (date.getHours() < 10 ? '0' : '' ) + date.getHours();
+        let minutes = (date.getMinutes() < 10 ? '0' : '' ) + date.getMinutes();
+        processedTrs.timestamp = `${day}/${month}/${date.getFullYear()} ${hours}:${minutes}`;
         processedTrs.recieve = trx.to === this.$store.state.accounts.activeAccount.getAddressString();
         processedTrs.address = processedTrs.recieve ? trx.from : trx.to;
         processedTrs.value = trx.value;
@@ -71,4 +72,7 @@ export default {
 </script>
 
 <style lang="scss">
+  .transactions {
+      max-width: 700px;
+  }
 </style>
