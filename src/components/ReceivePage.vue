b37c29b3c987abf501eadd07a3aa9389af2020d4
Антон Степаненков <Lenseg1@gmail.com>
2018-04-27 03:29:59 -0500

    recive page

--- src/components/ReceivePage.vue
+++ src/components/ReceivePage.vue
@@ -3,11 +3,39 @@
     <div class="section">
       <div class="container">
         <h1 class="title">Receive ETH</h1>
+        <p>{{address}}</p>
       </div>
     </div>
 
     <div class="section">
-      <div class="container">Incoming Payment History</div>
+      <div class="container">
+        <div class="">
+          <h2 class="subtitle">Incoming Payment History</h2>
+          <ul class="transactions">
+            <li class="box" v-for="transaction in processedTransactions">
+              <div class="columns">
+                <div class="column is-9">
+                  <h2 class="subtitle">
+                    <span>{{transaction.timestamp}}</span>
+                  </h2>
+                  <p>
+                    <span>From: </span>
+                    {{transaction.from}}
+                  </p>
+                </div>
+                <div class="column is-3">
+                  <p class="has-text-right">{{transaction.value}} ETH</p>
+                  <p class="has-text-right" v-if="transaction.timestamp && transaction.success">Succeeded</p>
+                  <p class="has-text-right" v-if="transaction.timestamp && !transaction.success">Failed</p>
+                </div>
+              </div>
+              <div class="columns">
+                <p class="column is-12">{{transaction.hash}}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,10 +44,54 @@
 export default {
   data () {
     return {
+      transactions: []
     }
+  },
+  computed: {
+    address() {
+      return this.$store.state.accounts.activeAccount.getAddressString()
+    },
+    processedTransactions() {
+      let arr = []
+      const sortedTransactions = this.transactions.sort((trx1, trx2) => {
+        if(typeof trx2.timestamp === 'undefined')
+          return 1
+        if(typeof trx1.timestamp === 'undefined')
+          return -1
+        return trx2.timestamp - trx1.timestamp;
+      });
+      return sortedTransactions.map(trx => {
+        let processedTrs = {};
+        if(trx.timestamp) {
+          const date = new Date(trx.timestamp*1000);
+          processedTrs.timestamp = date.toLocaleString();
+        }
+        processedTrs.value = trx.value;
+        processedTrs.hash = trx.hash;
+        if(trx.success)
+          processedTrs.success = trx.success;
+        return processedTrs;
+      })
+    }
+  },
+  created() {
+    const address = this.$store.state.accounts.activeAccount.getAddressString();
+    this.$http.get(`https://api.ethplorer.io/getAddressTransactions/${address}`, {
+      params: {
+        limit: 50,
+        apiKey: 'freekey'
+      }
+    }).then((resp) => {
+      this.transactions = resp.body.filter((trx) => {
+        return trx.to === this.address;
+      });
+    })
   }
 }
 </script>
 
 <style lang="scss">
+  .transactions {
+      max-width: 700px;
+  }
 </style>
