83e259780649db45d8ff645028609e144f4434f1
Dutch <dutch@unviral.com>
2018-05-02 22:22:54 -0400

    Show balance on home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="section">
-    <div class="container">
-      <h1 class="title">Endpass Wallet</h1>
-      <div class="accounts">
-
-        <div v-if="activeAccount">
+      <div class="home-page">
+        <div class="section" v-if="activeAccount">
+          <div class="container">
+            <h1 class="title">Your Endpass Wallet</h1>
+            <div class="address box">
+              <div class="level">
+                <div class="level-item is-inline-flex">
+                  <div>
+                    <p class="heading">Ethereum Address</p>
+                    <p
+                       class="is-size-4">{{activeAccount.getAddressString()}}</p>
+                  </div>
+                </div>
+                <div class="level-item">
+                  <div class="div">
+                    <p class="heading">Balance</p>
+                    <p class="stat title">{{balance}}</p>
+                    <p class="has-text-centered">ETH</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div v-else>
-          <p>Get started by generating or importing an Ethereum Wallet</p>
-          <router-link :to="{name: 'NewWallet'}" class="button is-primary">Create Wallet</router-link>
-          <p>Or</p>
-          <router-link :to="{name: 'ImportWallet'}" class="button is-primary">Import an existing wallet</router-link>
+
+        <div class="section" v-else>
+          <div class="container">
+            <p>Get started by generating or importing an Ethereum Wallet</p>
+            <router-link :to="{name: 'NewWallet'}" class="button is-primary">Create Wallet</router-link>
+            <p>Or</p>
+            <router-link :to="{name: 'ImportWallet'}" class="button is-primary">Import an existing wallet</router-link>
+          </div>
         </div>
 
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -27,6 +45,9 @@ export default {
   computed: {
     activeAccount() {
       return this.$store.state.accounts.activeAccount;
+    },
+    balance() {
+      return this.$store.state.accounts.balance === null ? null : web3.utils.fromWei(this.$store.state.accounts.balance);
     }
   }
 }
