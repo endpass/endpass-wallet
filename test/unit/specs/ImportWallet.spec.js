572b5eee5d33da793f774e0c64bd9335eb27f1e2
Антон Степаненков <Lenseg1@gmail.com>
2018-05-04 06:06:50 -0500

    tests works

--- test/unit/specs/ImportWallet.spec.js
+++ test/unit/specs/ImportWallet.spec.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import { shallow, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import VeeValidate from 'vee-validate'
 import ImportWallet from '../../../src/components/ImportWallet.vue'
 import EthWallet from 'ethereumjs-wallet'
 import HDKey from 'ethereumjs-wallet/hdkey'
 
 const localVue = createLocalVue()
+localVue.use(VeeValidate)
 
 localVue.use(Vuex)
 
@@ -23,6 +25,15 @@ describe('ImportWallet', () => {
     wrapper = shallow(ImportWallet, { store, localVue })
   })
 
+  it('sets the correct default data', () => {
+    expect(typeof ImportWallet.data).toBe('function')
+    const defaultData = ImportWallet.data()
+    expect(defaultData.privateKey).toBe('')
+    expect(defaultData.hdkeyPrase).toBe('')
+    expect(defaultData.privateKeyError).toBe(false)
+    expect(defaultData.hdkeyPraseError).toBe(false)
+  })
+
   it('correctly creates wallet with private key', () => {
     const vm = new Vue(ImportWallet).$mount()
     vm.privateKey = '4daf66f4ffed6d47e75d22e2c962d1f9a36550dc2cfda4bfb5da741bdc97d6ba';
@@ -32,6 +43,14 @@ describe('ImportWallet', () => {
     const vm = new Vue(ImportWallet).$mount()
     vm.hdkeyPrase = 'salt suit force stomach lounge endless soul junk join leg sort aware';
     expect(vm.createWalletWithPrase() instanceof EthWallet).toBe(true);
+  })
+
+  it('correctly sets error with bad data', () => {
+    const vm = new Vue(ImportWallet).$mount();
+    vm.addWalletWithKey();
+    vm.addWalletWithPrase();
+    expect(vm.privateKeyError).toBe(true);
+    expect(vm.hdkeyPraseError).toBe(true);
   })
 
   it('defaults to import by seed phrase', () => {
--- test/unit/specs/SentEther.spec.js
+++ test/unit/specs/SentEther.spec.js
@@ -1,18 +1,26 @@
 import Vue from 'vue'
+import { shallow, createLocalVue } from '@vue/test-utils'
+import VeeValidate from 'vee-validate'
+
 import SendEther from '../../../src/components/SendPage.vue'
 
+const localVue = createLocalVue()
+localVue.use(VeeValidate)
 
 describe('SendEther', () => {
+  let wrapper
+  beforeEach(() => {
+  wrapper = shallow(SendEther, { localVue })
+  })
   it('sets correct data', () => {
-    const vm = new Vue(SendEther).$mount();
-    vm.gasPrice = 10;
-    vm.gasLimit = 10;
-    vm.value = 10;
-    expect(vm.gasPrice).toBe('10');
-    expect(vm.gasLimit).toBe('10');
-    expect(vm.value).toBe('10');
-    expect(vm.transaction.gasPrice).toBe('0x2540be400');
-    expect(vm.transaction.gasLimit).toBe('0xa');
-    expect(vm.transaction.value).toBe('0x8ac7230489e80000');
+    wrapper.vm.gasPrice = 10;
+    wrapper.vm.gasLimit = 10;
+    wrapper.vm.value = 10;
+    expect(wrapper.vm.gasPrice).toBe('10');
+    expect(wrapper.vm.gasLimit).toBe('10');
+    expect(wrapper.vm.value).toBe('10');
+    expect(wrapper.vm.transaction.gasPrice).toBe('0x2540be400');
+    expect(wrapper.vm.transaction.gasLimit).toBe('0xa');
+    expect(wrapper.vm.transaction.value).toBe('0x8ac7230489e80000');
   })
 })
