4a3cba2c6eef58c4a5df68ee17ee37c9c922bc64
Dutch <dutch@unviral.com>
2018-05-04 17:57:53 -0400

    Organize new account modal markup

--- src/components/NewAccountModal.vue
+++ src/components/NewAccountModal.vue
@@ -4,16 +4,45 @@
       <template slot="header">Create New Address</template>
 
       <div v-if="!createdAccount">
+        <p class="subtitle">You currently have
+        <strong>{{accounts.length}}</strong> active addresses in your wallet.</p>
         <p class="subtitle">Click the button below to create an additional
       address you can use to receive Ethereum and tokens.</p>
-        <button class="button is-primary" @click="createNewAccount">Create
-          Address</button>
       </div>
       <div v-else>
-        <p class="subtitle">New Account Created</p>
-        <div class="box">
-          <span class="text-label">Address</span>
-          <span class="address">{{createdAccount.getAddressString()}}</span>
+        <p class="subtitle">New Address Created</p>
+
+        <div class="message">
+          <div class="message-header">
+            <p>Public Address</p>
+          </div>
+          <div class="message-body">
+            <p>Use this address to receive Ether and tokens.</p>
+            <p class="code address">{{createdAccount.getAddressString()}}</p>
+          </div>
+        </div>
+
+        <div class="message is-warning">
+          <div class="message-header">
+            <p>Private Key</p>
+          </div>
+          <div class="message-body">
+            <p class="bold">Save this for your records and DO NOT share it
+            with anyone!</p>
+            <p class="code">{{createdAccount.getPrivateKeyString()}}</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div slot="footer">
+        <div v-if="!createdAccount">
+          <a class="button is-primary" @click="createNewAccount">Create
+            New Address</a>
+          <a class="button" @click="close">Cancel</a>
+        </div>
+        <div v-else>
+          <a class="button is-primary" @click="close">Close</a>
         </div>
       </div>
 
@@ -62,4 +91,18 @@ export default {
 </script>
 
 <style lang="scss">
+.new-account-modal {
+  .bold {
+    font-weight: 700;
+    font-size: 1.1em;
+  }
+
+  .code {
+    background: $light-grey;
+    color: $dark-grey;
+    padding: 1em;
+    word-wrap: break-word;
+    font-family: "Lucida Console","Courier New",monospace;
+  }
+}
 </style>
