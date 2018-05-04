8d49017f0b4f90dd4dcf9c8a6ff0a65335a62e3b
Антон Степаненков <Lenseg1@gmail.com>
2018-05-04 05:11:19 -0500

    removed import valet validation tests

--- test/unit/specs/ImportWallet.spec.js
+++ test/unit/specs/ImportWallet.spec.js
@@ -23,15 +23,6 @@ describe('ImportWallet', () => {
     wrapper = shallow(ImportWallet, { store, localVue })
   })
 
-  it('sets the correct default data', () => {
-    expect(typeof ImportWallet.data).toBe('function')
-    const defaultData = ImportWallet.data()
-    expect(defaultData.privateKey).toBe('')
-    expect(defaultData.hdkeyPrase).toBe('')
-    expect(defaultData.privateKeyError).toBe(false)
-    expect(defaultData.hdkeyPraseError).toBe(false)
-  })
-
   it('correctly creates wallet with private key', () => {
     const vm = new Vue(ImportWallet).$mount()
     vm.privateKey = '4daf66f4ffed6d47e75d22e2c962d1f9a36550dc2cfda4bfb5da741bdc97d6ba';
@@ -41,14 +32,6 @@ describe('ImportWallet', () => {
     const vm = new Vue(ImportWallet).$mount()
     vm.hdkeyPrase = 'salt suit force stomach lounge endless soul junk join leg sort aware';
     expect(vm.createWalletWithPrase() instanceof EthWallet).toBe(true);
-  })
-
-  it('correctly sets error with bad data', () => {
-    const vm = new Vue(ImportWallet).$mount();
-    vm.addWalletWithKey();
-    vm.addWalletWithPrase();
-    expect(vm.privateKeyError).toBe(true);
-    expect(vm.hdkeyPraseError).toBe(true);
   })
 
   it('defaults to import by seed phrase', () => {
