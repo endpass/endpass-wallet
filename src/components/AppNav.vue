0a2d1513a7add93f5a02f10e8ddc45ccb82e5558
Dutch <dutch@unviral.com>
2018-05-21 22:32:09 -0400

    Refactor top nav to separate component

---
+++ src/components/AppNav.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="app-nav navbar">
+    <div class="navbar-brand">
+      <router-link class="navbar-item logo-icon" to="/">
+        <img src="@/img/logo-light.png" alt="Endpass Wallet">
+      </router-link>
+      <a class="navbar-burger" @click="toggleNavMenu" :class="{'is-active':navMenuActive}">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+    <div class="navbar-menu" :class="{'is-active':navMenuActive}">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/" @click.native="toggleNavMenu">
+          <span class="icon is-small"
+                v-html="require('@/img/home.svg')"></span>Dashboard
+        </router-link>
+        <router-link v-if="activeAccount" class="navbar-item" :to="{name: 'HistoryPage'}" @click.native="toggleNavMenu">
+          <span class="icon is-small"
+                v-html="require('@/img/clock.svg')"></span>History
+        </router-link>
+        <router-link v-if="activeAccount" class="navbar-item" :to="{name: 'SendPage'}" @click.native="toggleNavMenu">
+          <span class="icon is-small"
+                v-html="require('@/img/arrow-thick-left.svg')"></span>Send
+        </router-link>
+        <router-link v-if="activeAccount" class="navbar-item" :to="{name:
+        'ReceivePage'}" @click.native="toggleNavMenu">
+          <span class="icon is-small"
+                v-html="require('@/img/arrow-thick-right.svg')"></span>Receive
+        </router-link>
+        <router-link v-if="activeAccount" class="navbar-item" :to="{name:
+        'TokensPage'}" @click.native="toggleNavMenu">
+          <span class="icon is-small"
+                v-html="require('@/img/compass.svg')"></span>Tokens
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navMenuActive: false
+    }
+  },
+  methods: {
+    toggleNavMenu () {
+      this.navMenuActive = !this.navMenuActive
+    }
+  },
+  props: {
+    activeAccount: Boolean
+  }
+}
+</script>
+
+<style lang="scss">
+// Navbar
+.app-nav {
+  .navbar-menu {
+    a.navbar-item {
+      font-family: $heading-font-family;
+      font-size: 1.1rem;
+      text-transform: uppercase;
+    }
+  }
+
+  .navbar-burger {
+    span {
+      height: 3px;
+    }
+  }
+
+  a.navbar-item.is-active, a.navbar-item:hover, a.navbar-link.is-active,
+  a.navbar-link:hover, .router-link-exact-active {
+    background-color: initial;
+    color: $purple;
+
+    .icon svg {
+      fill: $purple;
+    }
+  }
+
+  .navbar-item .icon:only-child, .navbar-link .icon:only-child {
+    margin-left: 0;
+    margin-right: 0.25em;
+  }
+}
+</style>
