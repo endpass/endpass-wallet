944fd17fd4ceb5a86b247609d54bd1704438fe0b
Dutch <dutch@unviral.com>
2018-05-21 20:09:05 -0400

    Remove deprecated jest options

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -20,12 +20,4 @@ module.exports = {
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
-  coverageDirectory: '<rootDir>/test/unit/coverage',
-  collectCoverageFrom: [
-    'src/**/*.{js,vue}',
-    '!src/main.js',
-    '!src/router/index.js',
-    '!**/node_modules/**'
-  ]
 }
