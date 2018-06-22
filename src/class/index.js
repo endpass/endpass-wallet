3a62c087d024ab019ecd16bb02c525108b7a4a3c
Alexander Kazimir <alex_kazik@mail.ru>
2018-06-22 19:57:44 +0300

    Add error handling

--- src/class/index.js
+++ src/class/index.js
@@ -1,2 +1,4 @@
 export { providerFactory } from './provider';
 export { Transaction } from './Transaction';
+export { default as EventEmitter } from './EventEmitter';
+export { NotificationError } from './error';
