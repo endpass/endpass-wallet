3697ba76399cd9067e15a5a6883ba60a55ce9712
Dutch <dutch@unviral.com>
2018-08-02 14:43:30 -0400

    Add VPassword component

---
+++ src/components/ui/form/VPassword.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-input :type="inputType" autocomplete="current-password" v-bind="$attrs">
+  </v-input>
+</template>
+
+<script>
+import VInput from '@/components/ui/form/VInput.vue';
+
+// Wrap a VInput component to turn it into a toggleable password input
+export default {
+  props: {
+    // If true, the password is shown to the user
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isVisible: this.visible,
+    };
+  },
+  methods: {
+    toggleVisible() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+  computed: {
+    inputType() {
+      return this.isVisible ? 'text' : 'password';
+    },
+  },
+  components: {
+    VInput,
+  },
+};
+</script>
