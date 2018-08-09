541993142046e1974338374b1e63edb25e0ffd9a
Alex <alex_kazik@mail.ru>
2018-08-09 11:03:22 +0300

    Fix price input bug with zero eth price

--- test/unit/specs/components/pages/Send.spec.js
+++ test/unit/specs/components/pages/Send.spec.js
@@ -4,8 +4,12 @@ import web3 from 'web3';
 import { shallow, createLocalVue, mount } from '@vue/test-utils';
 import Notifications from 'vue-notification';
 import validation from '@/validation';
+import { Transaction } from '@/class';
+import { generateStubs } from '@/utils/testUtils';
 
 import Send from '@/components/pages/Send.vue';
+
+Transaction.prototype.getFullPrice = jest.fn().mockResolvedValue();
 
 const localVue = createLocalVue();
 
@@ -17,6 +21,7 @@ describe('Send', () => {
   let store;
   let wrapper;
   let web3Instance = new web3('https://mainnet.infura.io/');
+
   beforeEach(() => {
     store = new Vuex.Store({
       state: {
@@ -32,7 +37,7 @@ describe('Send', () => {
           settings: {
             fiatCurrency: 'USD',
           },
-          balance: null,
+          balance: '1000000000000000000',
           pendingTransactions: [
             {
               timestamp: 1524505925,
@@ -90,117 +95,149 @@ describe('Send', () => {
     wrapper = shallow(Send, { store, localVue });
   });
 
-  it('should validate data', async () => {
-    const getFullPrice = jest.fn().mockResolvedValue('400');
+  describe('behavior', () => {
+    it('should validate data', async () => {
+      const getFullPrice = jest.fn().mockResolvedValue('400');
 
-    wrapper = mount(Send, {
-      store,
-      localVue,
-      computed: {
-        maxAmount: () => 2,
-      },
-    });
-
-    const { errors, $nextTick } = wrapper.vm;
-
-    wrapper.setData({
-      transaction: {
-        to: '',
-        gasPrice: '900',
-        gasLimit: '2200000000',
-        data: 'asdfas',
-        getFullPrice,
-      },
-      value: '2.222222222222222222222222',
-    });
-
-    await wrapper.vm.$validator.validateAll();
-
-    expect(errors.first('gasPrice').includes('between')).toBeTruthy();
-    expect(errors.first('gasLimit').includes('between')).toBeTruthy();
-    expect(errors.first('price').includes('between')).toBeTruthy();
-    expect(errors.first('value').includes('decimal')).toBeTruthy();
-    expect(errors.first('data').includes('hex')).toBeTruthy();
-
-    wrapper.setData({
-      transaction: {
-        ...wrapper.vm.transaction,
-        to: '',
-        gasPrice: '91',
-        gasLimit: '22000',
-        // prettier-ignore
-        data: '0xa9059cbb000000000000000000000000b6ed7644c69416d67b522e20bc294a9a9b405b31000000000000000000000000000000000000000000000000000000003b9aca00'
-      },
-      value: '1.5',
-    });
-
-    wrapper.vm.$refs.address.$el.querySelector('input').value =
-      '0xE824633E6d247e64ba2cD841D8270505770d53fE';
-
-    await wrapper.vm.$validator.validateAll();
-
-    expect(errors.has('gasPrice')).toBeFalsy();
-    expect(errors.has('gasLimit')).toBeFalsy();
-    expect(errors.has('price')).toBeFalsy();
-    expect(errors.has('value')).toBeFalsy();
-    expect(errors.has('data')).toBeFalsy();
-
-    wrapper.setData({
-      transaction: {
-        ...wrapper.vm.transaction,
-        to: '123',
-        gasPrice: '-90',
-        gasLimit: '-22000',
-        data: '0x',
-      },
-      value: '-2.22222',
-    });
-
-    await wrapper.vm.$validator.validateAll();
-
-    expect(errors.first('gasPrice').includes('numeric')).toBeTruthy();
-    expect(errors.first('gasLimit').includes('numeric')).toBeTruthy();
-    expect(errors.first('price').includes('between')).toBeTruthy();
-    expect(errors.first('value').includes('between')).toBeTruthy();
-  });
-
-  it('should allow empty string in input', () => {
-    wrapper = mount(Send, {
-      store,
-      localVue,
-    });
-
-    const inputIdArr = ['address', 'gasPrice', 'gasLimit', 'value'];
-
-    inputIdArr
-      .map(inputId => wrapper.find(`#${inputId}`))
-      .forEach(inputElem => {
-        inputElem.element.value = '';
-        inputElem.trigger('input');
-        expect(inputElem.element.value).toBe('');
+      wrapper = mount(Send, {
+        store,
+        localVue,
+        computed: {
+          maxAmount: () => 2,
+        },
       });
-  });
 
-  it('should sync amount and price input', async () => {
-    wrapper = mount(Send, {
-      store,
-      localVue,
+      const { errors } = wrapper.vm;
+
+      wrapper.setData({
+        transaction: {
+          to: '',
+          gasPrice: '900',
+          gasLimit: '2200000000',
+          data: 'asdfas',
+          getFullPrice,
+        },
+        value: '2.222222222222222222222222',
+      });
+
+      await wrapper.vm.$validator.validateAll();
+
+      expect(errors.first('gasPrice').includes('between')).toBeTruthy();
+      expect(errors.first('gasLimit').includes('between')).toBeTruthy();
+      expect(errors.first('price').includes('between')).toBeTruthy();
+      expect(errors.first('value').includes('decimal')).toBeTruthy();
+      expect(errors.first('data').includes('hex')).toBeTruthy();
+
+      wrapper.setData({
+        transaction: {
+          ...wrapper.vm.transaction,
+          to: '',
+          gasPrice: '91',
+          gasLimit: '22000',
+          // prettier-ignore
+          data: '0xa9059cbb000000000000000000000000b6ed7644c69416d67b522e20bc294a9a9b405b31000000000000000000000000000000000000000000000000000000003b9aca00'
+        },
+        value: '1.5',
+      });
+
+      wrapper.vm.$refs.address.$el.querySelector('input').value =
+        '0xE824633E6d247e64ba2cD841D8270505770d53fE';
+
+      await wrapper.vm.$validator.validateAll();
+
+      expect(errors.has('gasPrice')).toBeFalsy();
+      expect(errors.has('gasLimit')).toBeFalsy();
+      expect(errors.has('price')).toBeFalsy();
+      expect(errors.has('value')).toBeFalsy();
+      expect(errors.has('data')).toBeFalsy();
+
+      wrapper.setData({
+        transaction: {
+          ...wrapper.vm.transaction,
+          to: '123',
+          gasPrice: '-90',
+          gasLimit: '-22000',
+          data: '0x',
+        },
+        value: '-2.22222',
+      });
+
+      await wrapper.vm.$validator.validateAll();
+
+      expect(errors.first('gasPrice').includes('numeric')).toBeTruthy();
+      expect(errors.first('gasLimit').includes('numeric')).toBeTruthy();
+      expect(errors.first('price').includes('between')).toBeTruthy();
+      expect(errors.first('value').includes('between')).toBeTruthy();
     });
 
-    const valueInput = wrapper.find('input#value');
-    const priceInput = wrapper.find('input#price');
+    it('should allow empty string in input', () => {
+      wrapper = mount(Send, {
+        store,
+        localVue,
+      });
 
-    valueInput.element.value = 2;
-    valueInput.trigger('input');
+      const inputIdArr = ['address', 'gasPrice', 'gasLimit', 'value'];
 
-    expect(wrapper.vm.price).toBe('800.00');
+      inputIdArr
+        .map(inputId => wrapper.find(`#${inputId}`))
+        .forEach(inputElem => {
+          inputElem.element.value = '';
+          inputElem.trigger('input');
+          expect(inputElem.element.value).toBe('');
+        });
+    });
 
-    priceInput.element.value = 1200;
-    priceInput.trigger('input');
+    it('should sync amount and price input', () => {
+      wrapper = mount(Send, {
+        store,
+        localVue,
+      });
 
-    expect(wrapper.vm.value).toBe('3.000000000000000000');
+      const valueInput = wrapper.find('input#value');
+      const priceInput = wrapper.find('input#price');
 
-    wrapper.vm.setMaxAmount();
-    expect(wrapper.vm.value).toBe(wrapper.vm.maxAmount.toString());
+      valueInput.element.value = 2;
+      valueInput.trigger('input');
+
+      expect(wrapper.vm.price).toBe('800.00');
+
+      priceInput.element.value = 1200;
+      priceInput.trigger('input');
+
+      expect(wrapper.vm.value).toBe('3.000000000000000000');
+    });
+
+    it('should set transaction value to the maximum amount', () => {
+      wrapper.vm.$validator.validate = jest.fn();
+
+      wrapper.vm.setMaxAmount();
+
+      const maxAmount = wrapper.vm.maxAmount.toString();
+
+      expect(wrapper.vm.transaction.value).toBe(maxAmount);
+      expect(wrapper.vm.value).toBe(maxAmount);
+    });
+
+    it('should disable price input when zero eth price', () => {
+      wrapper = shallow(Send, {
+        store,
+        localVue,
+        stubs: generateStubs(Send),
+      });
+
+      const inputElem = wrapper.find('v-input#price');
+
+      wrapper.setComputed({
+        ethPrice: 0,
+      });
+
+      expect(inputElem.attributes().disabled).toBeTruthy();
+
+      wrapper.setComputed({
+        ethPrice: 1,
+      });
+
+      expect(inputElem.attributes().disabled).toBeFalsy();
+    });
   });
 });
