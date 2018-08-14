1b350e69248cbb9a3e77b8f300c1b291b9250ce5
Dutch <dutch@unviral.com>
2018-08-13 21:15:14 -0400

    Add nav sidebar test

---
+++ test/e2e/specs/nav_sidebar.spec.js
@@ -0,0 +1,27 @@
+describe('Navigation sidebar', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit('#/');
+  });
+
+  it('should show sidebar controls', () => {
+    cy.get('.nav-sidebar').within(() => {
+      cy.contains('Logout');
+
+      // Network select
+      cy.get('.net-select .multiselect');
+      cy.get('.provider-select .multiselect');
+
+      // Account select
+      cy.get('.account-chooser .multiselect');
+
+      // Navigation
+      cy.get('a[href$="/history"]');
+      cy.get('a[href$="/send"]');
+      cy.get('a[href$="/receive"]');
+      cy.get('a[href$="/tokens"]');
+      cy.get('a[href$="/settings"]');
+      cy.get('a[href$="/message"]');
+    });
+  });
+});
