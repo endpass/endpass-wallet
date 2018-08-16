9cecd87e4f75dd7ca0acb539fa175bd3ff0331ec
Dutch <dutch@unviral.com>
2018-08-16 00:51:48 -0400

    Add full page loader component

---
+++ src/components/ui/PageLoader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-if="isLoading" class="page-loader modal is-active">
+    <div class="spinner">
+      <div class="spinner-dot spinner-dot-1">
+      </div>
+      <div class="spinner-dot spinner-dot-2">
+      </div>
+      <div class="spinner-dot spinner-dot-3">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Loading indicator that covers the entire page
+export default {
+  name: 'page-loader',
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.page-loader {
+  background-color: $purple;
+
+  .spinner {
+    color: $white;
+
+    .spinner-dot {
+      width: 18px;
+      height: 18px;
+      background-color: $white;
+
+      border-radius: 100%;
+      display: inline-block;
+      animation: bouncedelay 1.4s infinite ease-in-out both;
+    }
+
+    .spinner-dot-1 {
+      animation-delay: -0.32s;
+    }
+    .spinner-dot-2 {
+      animation-delay: -0.32s;
+    }
+    .spinner-dot-3 {
+      animation-delay: -0.16s;
+    }
+  }
+  @keyframes bouncedelay {
+    0%,
+    80%,
+    100% {
+      transform: scale(0);
+    }
+    40% {
+      transform: scale(1);
+    }
+  }
+}
+</style>
