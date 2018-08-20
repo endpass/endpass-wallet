3aaaca32a252a5a6f03ae39b4c05972a7209a009
m-russkikh <vyatich23-61@yandex.ru>
2018-08-20 14:24:25 +0300

    Added git revision in HTML meta tag

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -83,6 +83,9 @@ const webpackConfig = merge(baseWebpackConfig, {
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       // chunksSortMode: 'dependency',
+      meta: {
+        build: utils.getCommitHash(),
+      },
     }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
