b3d33329a79c22e3481b16bda2d56ffb6d7c942f
Антон Степаненков <Lenseg1@gmail.com>
2018-08-27 15:04:55 +0300

    price refactor update

--- test/unit/specs/store/price/mutations.spec.js
+++ test/unit/specs/store/price/mutations.spec.js
@@ -9,33 +9,42 @@ import {
 
 describe('price mutations', () => {
   const state = {};
-  it('should set price', () => {
-    const price = 0;
-    mutations[SET_PRICE](state, price);
-    expect(state.price).toBe(price);
+  describe(SET_PRICE, () => {
+    it('should set price', () => {
+      const price = 0;
+      mutations[SET_PRICE](state, price);
+      expect(state.price).toBe(price);
+    });
+  });
+  describe(SET_UPDATE_TIME, () => {
+    it('should set update time', () => {
+      const updateTime = 0;
+      mutations[SET_UPDATE_TIME](state, updateTime);
+      expect(state.updateTime).toBe(updateTime);
+    });
   });
 
-  it('should set update time', () => {
-    const updateTime = 0;
-    mutations[SET_UPDATE_TIME](state, updateTime);
-    expect(state.updateTime).toBe(updateTime);
+  describe(START_LOADING, () => {
+    it('should set loading to true', () => {
+      const updateTime = 0;
+      mutations[START_LOADING](state);
+      expect(state.isLoading).toBe(true);
+    });
   });
 
-  it('should set loading to true', () => {
-    const updateTime = 0;
-    mutations[START_LOADING](state);
-    expect(state.isLoading).toBe(true);
+  describe(STOP_LOADING, () => {
+    it('should set loading to false', () => {
+      const updateTime = 0;
+      mutations[STOP_LOADING](state);
+      expect(state.isLoading).toBe(false);
+    });
   });
 
-  it('should set loading to false', () => {
-    const updateTime = 0;
-    mutations[STOP_LOADING](state);
-    expect(state.isLoading).toBe(false);
-  });
-
-  it('should set interval', () => {
-    const interval = 0;
-    mutations[SET_INTERVAL](state, interval);
-    expect(state.interval).toBe(interval);
+  describe(SET_INTERVAL, () => {
+    it('should set interval', () => {
+      const interval = 0;
+      mutations[SET_INTERVAL](state, interval);
+      expect(state.interval).toBe(interval);
+    });
   });
 });
