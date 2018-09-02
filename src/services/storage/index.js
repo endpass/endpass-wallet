8c38dbf931d5539655a38170c8a2a501f5ea3801
Alex <alex_kazik@mail.ru>
2018-09-03 00:30:49 +0300

    Remove the storage service and mixin

--- src/services/storage/index.js
+++
@@ -1,7 +0,0 @@
-import Storage from './Storage';
-import LocalStorage from './LocalStorage';
-import ServerStorage from './ServerStorage';
-
-export const localStore = new LocalStorage();
-export const serverStore = new ServerStorage();
-export default new Storage(localStore, serverStore);
