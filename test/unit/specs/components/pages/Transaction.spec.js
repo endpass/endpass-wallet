e4321e4b48db709c6d205df361304b2a2ebb34d5
lamartire <lamartire@gmail.com>
2018-09-21 15:34:57 +0300

    Add some tests for privatePage mixins
    Add tests for pages with that mixin
    Fix brocken tests

--- test/unit/specs/components/pages/Transaction.spec.js
+++ test/unit/specs/components/pages/Transaction.spec.js
@@ -1,13 +1,31 @@
-import { shallow } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { shallow, createLocalVue } from '@vue/test-utils';
 import Transaction from '@/components/pages/Transaction';
 import { generateStubs } from '@/utils/testUtils';
 
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
 describe('Transaction page', () => {
   let wrapper;
+  let store;
 
   beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        accounts: {
+          namespaced: true,
+          getters: {
+            isPublicAccount: () => true,
+          },
+        },
+      },
+    });
     wrapper = shallow(Transaction, {
       stubs: generateStubs(Transaction),
+      localVue,
+      store,
     });
   });
 
