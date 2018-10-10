0635494b1b6c568f9cf7fbf2266e9c9e7990a9ca
Anton Stepanenkov <Lenseg1@gmail.com>
2018-10-10 16:21:15 +0300

    Add Inpage provider (#264)

---
+++ src/class/provider/InpageProvider.js
@@ -0,0 +1,73 @@
+import { EventEmitter } from '@/class';
+import { INPAGE_EVENT } from '@/constants';
+
+export default class InpageProvider {
+  constructor(eventEmitter) {
+    if (!eventEmitter instanceof EventEmitter)
+      throw new Error("Event emitter isn't provided");
+    eventEmitter.on(INPAGE_EVENT.SETTINGS, payload => {
+      this.updateSettings(payload);
+    });
+    eventEmitter.on(INPAGE_EVENT.RESPONSE, payload => {
+      this.handleResponse(payload);
+    });
+    this.eventEmitter = eventEmitter;
+    this.pendingRequestsHandlers = {};
+    this.settings = {};
+  }
+
+  handleResponse({ error, result }) {
+    this.pendingRequestsHandlers[result.id](error, result);
+    delete this.pendingRequestsHandlers[result.id];
+  }
+
+  updateSettings({ selectedAddress, networkVersion }) {
+    if (typeof selectedAddress !== 'undefined') {
+      this.settings.selectedAddress = selectedAddress;
+    }
+
+    if (typeof networkVersion !== 'undefined') {
+      this.settings.networkVersion = networkVersion;
+    }
+  }
+
+  sendAsync(payload, callback) {
+    this.pendingRequestsHandlers[payload.id] = callback;
+    this.eventEmitter.emit(INPAGE_EVENT.REQUEST, payload);
+  }
+
+  isConnected() {
+    return true;
+  }
+
+  send(payload) {
+    let result = null;
+    switch (payload.method) {
+      case 'eth_accounts':
+        result = this.settings.selectedAddress
+          ? [this.settings.selectedAddress]
+          : [];
+        break;
+
+      case 'eth_coinbase':
+        result = this.settings.selectedAddress || null;
+        break;
+
+      case 'eth_uninstallFilter':
+        this.sendAsync(payload, () => {});
+        result = true;
+        break;
+
+      case 'net_version':
+        result = this.settings.networkVersion || null;
+        break;
+    }
+
+    // return the result
+    return {
+      id: payload.id,
+      jsonrpc: payload.jsonrpc,
+      result: result,
+    };
+  }
+}
