881b1e3bcf65a82226f64a153c42e9731f70e2b5
Alex <alex_kazik@mail.ru>
2018-11-20 13:33:41 +0300

    Fix Trezor block popup for message signing
    fix pr

--- src/class/address.js
+++ src/class/address.js
@@ -59,17 +59,30 @@ export default class Address {
     return toChecksumAddress(this.getAddressString());
   }
 
-  // (Message) => Promise<SignedMessage>
+  /**
+   * Return signed message object
+   * @param {String} message Message for signing
+   * @return {Promise<Object<SignedMessage>>} Return signed message object
+   */
   sign(message) {
     return this.signStrategy.sign(message, this.info.index);
   }
 
-  // (Transaction) => Promise<SignedTrx>
+  /**
+   * Return signed transaction hash
+   * @param {Transaction} transaction Transaction instance
+   * @return {String<SignedTrxHash>} Resolve signed transaction hash
+   */
   signTransaction(transaction) {
     return this.signStrategy.signTransaction(transaction, this.info.index);
   }
 
-  // (String, String<Signature>) => Promise<Address>
+  /**
+   * Recover account address from signed message/hash
+   * @param {String} message Message/hash for signing
+   * @param {String<Signature>} signature Signature from signing
+   * @return {Promise<Address>} Resolve account address
+   */
   recover(message, signature) {
     return this.signStrategy.recover(message, signature);
   }
