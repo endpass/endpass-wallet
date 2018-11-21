a48461d79894b5885fafcd095c03c93ad88c57d7
lamartire <lamartire@gmail.com>
2018-11-21 11:56:39 +0300

    Add ajv schema to gas price service data

--- src/services/gas-price.js
+++ src/services/gas-price.js
@@ -1,12 +1,23 @@
-import { http } from '@/class/singleton';
+import { http, ajv } from '@/class/singleton';
 import { NotificationError } from '@/class';
+import gasPriceSchema from '@/schema/gasPrice';
 
-export default {
-  getGasPrice(address) {
+const gasPriceService = {
+  validateGasPrice(data) {
+    return new Promise(
+      (resolve, reject) =>
+        gasPriceSchema.gasPrice(data)
+          ? resolve(data)
+          : reject(new Error(ajv.errorsText(gasPriceSchema.gasPrice))),
+    );
+  },
+
+  getGasPrice() {
     return http
       .get(`${ENV.cryptoDataAPIUrl}/gas/price`)
-      .then(resp => resp.data)
-      .catch(e => {
+      .then(res => gasPriceService.validateGasPrice(res.data))
+      .then(res => res)
+      .catch(() => {
         throw new NotificationError({
           title: 'Failed to get suggested gas price',
           text:
@@ -16,3 +27,5 @@ export default {
       });
   },
 };
+
+export default gasPriceService;
