68162555d800f5a4ad6e335f1a46341adb2c778b
dudiq <idudiq@gmail.com>
2019-01-09 15:57:29 +0300

    Move abi to class

--- test/unit/specs/class/singleton/httpIdentity.spec.js
+++ test/unit/specs/class/singleton/httpIdentity.spec.js
@@ -1,6 +1,5 @@
 import { httpIdentity } from '@/class/singleton';
 import store from '@/store';
-import { REQUEST_TIMEOUT_MSEC } from '@/class/constants';
 
 jest.mock('@/store', () => ({
   dispatch: jest.fn(),
