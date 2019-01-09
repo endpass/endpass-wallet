bf86231cd653db286c64f3d4726c7241233041f6
dudiq <idudiq@gmail.com>
2019-01-09 16:10:19 +0300

    Fix rebase conflicts

--- src/class/internal.js
+++ src/class/internal.js
@@ -3,7 +3,7 @@ export { default as EventEmitter } from './EventEmitter';
 
 export { default as web3 } from './singleton/web3';
 
-export { default as Transaction } from './Transaction';
+export { default as Transaction } from './transaction/Transaction';
 export { NotificationError } from './error';
 export { default as Wallet } from './wallet';
 export { default as Token } from './Token';
