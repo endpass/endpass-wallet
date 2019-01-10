0a565f5562b2a6ac021bd6062e11f431298ec227
dudiq <idudiq@gmail.com>
2019-01-10 16:37:33 +0300

    Add core/class pack

--- src/class/singleton/web3.js
+++ src/class/singleton/web3.js
@@ -1,6 +1,6 @@
 // Wrapper around global web3 instance
-import { ProviderFactory } from '@/class/provider';
-import Web3Factory from '@/class/web3/Web3Factory';
+import Web3Factory from '@endpass/class/web3/Web3Factory';
+import { ProviderFactory } from '@endpass/class/provider';
 import { DEFAULT_NETWORKS } from '@/constants';
 
 const defaultProvider = ProviderFactory.create(DEFAULT_NETWORKS[0].url);
