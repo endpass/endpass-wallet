5b0e27300a7b9e7034bb15b2023394f0b6c1ff7c
dudiq <idudiq@gmail.com>
2019-01-11 12:57:07 +0300

    Add create injected classes

--- src/class/index.js
+++ src/class/index.js
@@ -1,9 +1,22 @@
 // external point for other modules
+import {
+  createERC20TokenClass,
+  createENSClass,
+  createWalletClass,
+  createTransactionClass,
+} from '@endpass/class';
+import web3 from './singleton/web3';
+
+const ERC20Token = createERC20TokenClass(web3);
+const ENSResolver = createENSClass(web3);
+const Wallet = createWalletClass(web3);
+const Transaction = createTransactionClass(web3);
+
+export { web3, ERC20Token, ENSResolver, Wallet, Transaction };
 
 export {
   default as EventEmitter,
   ProviderFactory,
-  default as web3,
   default as dappBridge,
   default as ajv,
   default as http,
@@ -11,12 +24,8 @@ export {
   LocalStorage,
   SettingsStorage,
   default as TransactionFactory,
-  default as Transaction,
   NotificationError,
-  default as Wallet,
-  default as ENSResolver,
   default as Token,
-  default as ERC20Token,
   TrezorProxy,
   LedgerProxy,
   HDProxy,
