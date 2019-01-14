15cdbed84d30270b48ac34d8eaa1d53b74c27f78
dudiq <idudiq@gmail.com>
2019-01-14 15:17:31 +0300

    refactor: Move Transaction with factory

--- src/class/proxy-index.js
+++ src/class/proxy-index.js
@@ -1,21 +1,19 @@
 export { createENSClass } from './ens';
 export { createWalletClass } from './wallet';
-export { createTransactionClass } from './transaction/Transaction';
 export { createERC20TokenClass } from './erc20';
 
 export { ProxyRequest } from './singleton';
 
 export { default as dappBridge } from './singleton/dappBridge';
 export {
+  createTransactionClass,
   LocalStorage,
   SettingsStorage,
   ProviderFactory,
   InpageProvider,
+  TransactionFactory,
 } from '@endpass/class';
 
-export {
-  default as TransactionFactory,
-} from './transaction/TransactionFactory';
 export { TrezorProxy } from './wallet/proxy';
 export { LedgerProxy } from './wallet/proxy';
 export { HDProxy } from './wallet/proxy';
