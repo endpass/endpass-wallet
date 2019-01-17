fe39b301eaba8122e6564881acaece225ad542f1
dudiq <idudiq@gmail.com>
2019-01-17 14:57:05 +0300

    refactor: Change mock for wallet proxy loading

--- test/unit/mocks/class/index.js
+++ test/unit/mocks/class/index.js
@@ -1,3 +1,5 @@
+import { loadProxy } from '@endpass/class';
+
 jest.mock('@/class', () => {
   const web3 = require.requireActual('@/class/singleton/web3').default;
   const erc20 = require.requireActual('./erc20').default;
@@ -5,8 +7,24 @@ jest.mock('@/class', () => {
 
   const originClass = require.requireActual('@/class');
 
+  const proxies = {
+    TrezorProxy: {
+      getNextWallets: jest.fn(),
+    },
+    LedgerProxy: {
+      getNextWallets: jest.fn(),
+    },
+    HDProxy: {
+      getNextWallets: jest.fn(),
+    },
+  };
+
+  const loadProxy = proxy => proxy;
+
   return {
     ...originClass,
+    proxies,
+    loadProxy,
     web3,
     ENSResolver: ens,
     ERC20Token: erc20,
