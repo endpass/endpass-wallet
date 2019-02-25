86e7786ac15c14d99561dd447735c6b27338f32f
dudiq <idudiq@gmail.com>
2019-02-25 09:53:43 +0300

    fix: update after env fix in core/class and core/utils

--- src/class/index.js
+++ src/class/index.js
@@ -3,16 +3,14 @@ import {
   createERC20TokenClass,
   createENSClass,
   createWalletClass,
-  createTransactionClass,
 } from '@endpass/class';
 import web3 from './singleton/web3';
 
 const ERC20Token = createERC20TokenClass(web3);
 const ENSResolver = createENSClass(web3);
 const Wallet = createWalletClass(web3);
-const Transaction = createTransactionClass(web3);
 
-export { web3, ERC20Token, ENSResolver, Wallet, Transaction };
+export { web3, ERC20Token, ENSResolver, Wallet };
 
 export { default as ajv } from './singleton/ajv';
 export { default as http } from './singleton/http';
@@ -26,4 +24,5 @@ export {
   NotificationError,
   Token,
   EventEmitter,
+  Transaction,
 } from '@endpass/class';
