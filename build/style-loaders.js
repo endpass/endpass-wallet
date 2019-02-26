3f329fda6fb0dac694dcd27c8be88c036a7540cf
Mona Lisa <Lenseg1@gmail.com>
2019-02-26 17:38:12 +0530

    (refactor): build utils from package

--- build/style-loaders.js
+++ build/style-loaders.js
@@ -1,24 +1,5 @@
-'use strict';
 const path = require('path');
-const config = require('../config');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const packageConfig = require('../package.json');
-
-function exec(cmd) {
-  return require('child_process')
-    .execSync(cmd)
-    .toString()
-    .trim();
-}
-
-exports.assetsPath = function(_path) {
-  const assetsSubDirectory =
-    process.env.NODE_ENV === 'production'
-      ? config.build.assetsSubDirectory
-      : config.dev.assetsSubDirectory;
-
-  return path.posix.join(assetsSubDirectory, _path);
-};
 
 exports.cssLoaders = function(options) {
   options = options || {};
@@ -96,25 +77,3 @@ exports.styleLoaders = function(options) {
 
   return output;
 };
-
-exports.createNotifierCallback = () => {
-  const notifier = require('node-notifier');
-
-  return (severity, errors) => {
-    if (severity !== 'error') return;
-
-    const error = errors[0];
-    const filename = error.file && error.file.split('!').pop();
-
-    notifier.notify({
-      title: packageConfig.name,
-      message: severity + ': ' + error.name,
-      subtitle: filename || '',
-      icon: path.join(__dirname, 'logo.png'),
-    });
-  };
-};
-
-exports.exec = exec;
-
-exports.getCommitHash = () => exec('git rev-parse --short HEAD');
