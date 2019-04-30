2363740f05faa64fde9947e6c0027409d6f85994
lamartire <lamartire@gmail.com>
2019-04-30 19:32:18 +0300

    chore: update connect and prevent widget auto-mounting

--- src/class/singleton/connect.js
+++ src/class/singleton/connect.js
@@ -3,6 +3,7 @@ import EndpassConnect from '@endpass/connect';
 const connect = new EndpassConnect({
   authUrl: ENV.VUE_APP_CONNECT_URL,
   isIdentityMode: true,
+  widget: false,
 });
 
 export default connect;
