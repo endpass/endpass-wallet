66f831c2c8f104d557d770d4bf3279a8befdf1bd
idudiq <idudiq@gmail.com>
2019-06-11 15:23:18 +0300

    feat: update connect dep and others

--- src/class/singleton/connect.js
+++ src/class/singleton/connect.js
@@ -4,6 +4,7 @@ const connect = new EndpassConnect({
   authUrl: ENV.VUE_APP_CONNECT_URL,
   isIdentityMode: true,
   widget: false,
+  oauthClientId: 'should_replace_by_real_token',
 });
 
 export default connect;
