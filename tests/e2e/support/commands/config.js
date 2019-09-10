e0165a3110be17ddb599d9b4fba3d1a518f598b0
Alexander Kazimir <alex_kazik@mail.ru>
2019-09-10 12:49:30 +0300

    fix: fix

--- tests/e2e/support/commands/config.js
+++ tests/e2e/support/commands/config.js
@@ -1,5 +1,7 @@
 export const identityAPIUrl = 'https://identity-dev.endpass.com/api/v1.1';
-export const cryptodataAPIUrl = '/cryptodata/api/v1.1';
+export const cryptodataAPIUrl = 'https://cryptodata-dev.endpass.com/api/v1.1';
+export const tokeninfoAPIUrl = 'https://tokeninfo-dev.endpass.com/api/v1';
+export const tokenImageAPIUrl = 'https://tokeninfo-dev.endpass.com/image';
 export const mainNetworkId = 1;
 
 export default {
